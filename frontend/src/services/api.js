ae3d9e4ddb6033bf4f1ce4e8d4ab1fb2395a4e44
radeshsai <radeshsai08@gmail.com>
2026-04-01 14:45:22 +0530

    fixed API baseURL and registration issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import useAuthStore from '../store/authStore'
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: import.meta.env.VITE_API_URL ,
   headers: { 'Content-Type': 'application/json' },
 })
 
